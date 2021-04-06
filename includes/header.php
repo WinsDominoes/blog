253e8f6bf707accca471a8edf60da3d9f1c69021
WinsDominoes <49825651+WinsDominoes@users.noreply.github.com>
2021-04-06 07:32:24 +0700

    HELLO

---
+++ includes/header.php
@@ -0,0 +1,38 @@
+<?php
+    include("config.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Win's Dominoes - Personal Website</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <div id="header_1">
+            <h1>Win's Dominoes Personal Blog</h1>
+            <span>Welcome, to the site for Win's personal blogs!</span>
+        </div>
+
+        <div id="header_2">
+            <h3>Well, people ask me if I use PHP for coding websites of my own.
+            <br>The answer is YES and they would probably get mad at me.</h3>
+        </div>
+
+        <div id="menu">
+            <div id="nav">
+                <h5>Web Directory</h5>
+                <nav>
+                    <a href="/blogs/">Blogs</a> |
+                    <a href="/opinions/">Opinions</a> |
+                    <a href="/programming/">Programming</a> |
+                    <a href="/about/">About</a>
+                </nav>
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    include("includes/header.php");
+?>
+
+<div class="content">
+    
+</div>
+
+<?php
+    include("includes/footer.php");
+?>
